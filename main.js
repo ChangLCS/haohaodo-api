0ea73ebee4b98d7584d6f5e9fbbabec3fde38782
Chang <584911924@qq.com>
2019-03-10 10:22:36 +0800

    测试一下看ip

--- main.js
+++ main.js
@@ -23,29 +23,31 @@ app.use(
 );
 
 //  打印ip，顺便验一下token
-app.use(async (ctx, next) => {
-  logger.trace(`${ctx.ip}----${ctx.url}`);
+// app.use(async (ctx, next) => {
+//   logger.trace(`${ctx.ip}----${ctx.url}`);
 
-  // if (router.noTokenPath.indexOf(ctx.path) === -1) {
-  //   const token = ctx.query.accessToken;
-  //   await jwt.verify(token, 'secret', async (err) => {
-  //     if (err) {
-  //       logger.error('token 失效', err);
-  //       ctx.body = config.setResponseError('没有token或者token已过期', 401);
-  //     } else {
-  //       await next();
-  //     }
-  //   });
-  // } else {
-  await next();
-  // }
-});
+// if (router.noTokenPath.indexOf(ctx.path) === -1) {
+//   const token = ctx.query.accessToken;
+//   await jwt.verify(token, 'secret', async (err) => {
+//     if (err) {
+//       logger.error('token 失效', err);
+//       ctx.body = config.setResponseError('没有token或者token已过期', 401);
+//     } else {
+//       await next();
+//     }
+//   });
+// } else {
+// await next();
+// }
+// });
 
 app.use(bodyParser());
 app.use(router.router.routes());
 app.use(router.router.allowedMethods());
 
 app.use(async (ctx, next) => {
+  logger.trace(`${ctx.ip}----${ctx.url}`);
+  logger.debug(ctx);
   await next();
 });
 
